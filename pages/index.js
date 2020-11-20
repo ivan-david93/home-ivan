536765f8963b0f0cbbabb9f7dbece1ede3df1949
NB24200 <ivan.david@celfocus.com>
2020-11-20 14:56:19 +0000

    #ID Fix react component

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 var __html = require('./template');
 var template = { __html: __html };
 
-class Home extends Component {
+class Home extends React.Component {
   render() {
     return (
       <div className="home">
